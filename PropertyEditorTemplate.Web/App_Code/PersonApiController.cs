67e8c8245bb4301275749bca1dc86375e86a4964
Tomas Foss Renaa <tomasrenaa@gmail.com>
2020-10-17 14:55:01 +0200

    create property editor docs walkthrough 4/X
    https://our.umbraco.com/documentation/Tutorials/Creating-a-Property-Editor/part-4
    - not working
    The specified table does not exist. [ people ]  | while using a .sdf db

---
+++ PropertyEditorTemplate.Web/App_Code/PersonApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Umbraco.Web.WebApi;
+using Umbraco.Web.Editors;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Scoping;
+
+
+namespace My.Controller
+{
+    [Umbraco.Web.Mvc.PluginController("My")]
+    public class PersonApiController : UmbracoAuthorizedJsonController
+    {
+        private readonly IScopeProvider scopeProvider;
+        public PersonApiController(IScopeProvider scopeProvider)
+        {
+            this.scopeProvider = scopeProvider;
+        }
+        public IEnumerable<Person> GetAll()
+        {
+            using (var scope = scopeProvider.CreateScope(autoComplete: true))
+            {
+                // TODO FIX
+                //var sql = scope.SqlContext.Sql().Select("*").From("people");
+                //return scope.Database.Fetch<Person>(sql);  // this fails, maybe bcause of sdf limitations? 
+
+                return new List<Person>();
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+    }
+}
