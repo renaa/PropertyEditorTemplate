a0d2b55a5f502ac649a1b69705d47f6d7dc5936a
Tomas Foss Renaa <tomasrenaa@gmail.com>
2020-10-18 23:01:51 +0200

    ✨ giphy property editor

---
+++ PropertyEditorTemplate/App_Plugins/Giphy/js/giphyPicker.controller.js
@@ -0,0 +1,39 @@
+﻿function giphyPickerController($scope, $http, $element) {
+    setTimeout(function () {
+        var input = $element[0].querySelector("#giphy-search");
+        if (input) input.focus();
+    }, 20);
+
+    $scope.submit = function (result) {
+        if ($scope.model.submit) {
+            $scope.model.submit(result);
+        }
+    }
+    $scope.close = function () {
+        if ($scope.model.close) {
+            $scope.model.close();
+        }
+    }
+
+    $scope.search = function () {
+        $scope.model.loading = true;
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${$scope.model.apiKey}&q=${$scope.model.search}`;
+        $http({
+            method: 'GET',
+            url: url,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then(function (response) {
+            if (response !== null && response !== undefined && response.data !== undefined) {
+                $scope.model.results = response.data.data;
+            }
+            else {
+                $scope.model.results = null;
+            }
+            $scope.model.loading = false;
+        }
+        )
+    }
+}
+angular.module("umbraco").controller("My.GiphyPickerController", giphyPickerController);
---
+++ PropertyEditorTemplate/App_Plugins/Giphy/js/giphyPropertyEditor.controller.js
@@ -0,0 +1,22 @@
+﻿function giphyPropertyEditorController($scope, editorService) {
+    $scope.add = function () {
+        var giphyPicker = {
+            apiKey: $scope.model.config.apiKey,
+            title: "Animated Gif Search",
+            view: "/App_Plugins/Giphy/giphyPicker.html",
+            size: "medium",
+            submit: function (model) {
+                $scope.model.value = model.images.original.url; 
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+        editorService.open(giphyPicker);
+    };
+    $scope.remove = function () {
+        $scope.model.value = null;
+    }
+}
+angular.module("umbraco").controller("My.GiphyPropertyEditorController", giphyPropertyEditorController);
